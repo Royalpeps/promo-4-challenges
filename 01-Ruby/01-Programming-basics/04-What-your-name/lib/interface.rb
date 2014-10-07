f63a0de473432674f5d850d8854b985f0fb9676e
amontcoudiol <amontcoudiol@gmail.com>
2014-10-07 14:55:55 +0200

    exo 4 terminé

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,5 +1,17 @@
 require_relative "compute_name"
 
+puts "Enter your first name"
+fn = gets.chomp
+
+puts "Enter your middle name"
+mn = gets.chomp
+
+puts "Enter your last name"
+ln = gets.chomp
+
+puts "Hello #{compute_name(fn, mn, ln)}"
+
+
 # TODO: ask for the first name
 # TODO: ask for the middle name
 # TODO: ask for the last name
