3414d6d759ff8139b5f6f077c02d8f337866deeb
amontcoudiol <amontcoudiol@gmail.com>
2014-10-10 15:57:47 +0200

    yo

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,37 @@
+# def array_to_hash(array)
+#   #TODO: implement the method :)
+#   hash = {}
+#   array.each_with_index {|a,index| hash[index] = a }
+#   hash
+# end
+
+# p array_to_hash(["coucou", "cest", "moi"])
+
+
+
+
 def array_to_hash(array)
   #TODO: implement the method :)
+  hash = {}
+  array.each_with_index {|a,index|
+    index = yield(index) if block_given?
+    hash[index] = a }
+  hash
 end
+
+p array_to_hash(["coucou", "cest", "moi"]) { |index|
+  "key#{index + 1}" }
+
+
+# def array_to_hash(array)
+#   #TODO: implement the method :)
+#   hash = {}
+#   array.each_with_index {|a|
+#     index = yield
+#     hash[index] = a }
+#   hash
+# end
+
+# p array_to_hash(["coucou", "cest", "moi"]) do |index|
+#   "key#{index + 1}"
+# end
