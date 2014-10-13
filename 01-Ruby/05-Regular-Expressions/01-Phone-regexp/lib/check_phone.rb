27bfc07ae95af00260b2848c12e5fc66447390b6
amontcoudiol <amontcoudiol@gmail.com>
2014-10-13 11:37:00 +0200

    exo termine

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,34 @@
+# def french_phone_number?(phone_number)
+#   # TODO: true or false?
+#   nb_string = phone_number.tr_s(" ","").tr_s("-","")
+#   if nb_string.length == 10 && nb_string[0] == "0" && nb_string[1] != "0"
+#     return true
+#   elsif nb_string.length == 12 && nb_string[0,3] == "+33"
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+
+
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  nb_string = phone_number.tr_s(" ","").tr_s("-","")
+  if nb_string =~ /^(0)[^0](\d{8})$/
+    return true
+  elsif nb_string =~ /^\W33(\d{9})$/
+    return true
+  else
+    return false
+  end
 end
+
+
+
+
+
+
+puts french_phone_number?("0665363636")
+puts french_phone_number?("06 65 36 36")
+puts french_phone_number?("+33 6 19 57 79 71")
