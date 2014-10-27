ef45212a73712ae7be34f4d9199b8446656785ad
amontcoudiol <amontcoudiol@gmail.com>
2014-10-27 15:44:44 +0100

    exo termine

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,11 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+100.times do
+  name = Faker::Internet.domain_word
+  url = Faker::Internet.url
+  votes = 0
+  post = Post.new(name: name, url: url, votes: votes)
+  post.save
+end
+
