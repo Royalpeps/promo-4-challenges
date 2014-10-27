e4f5ecd9d98fc19d7d01827eba3edd49d40e4bed
amontcoudiol <amontcoudiol@gmail.com>
2014-10-27 15:17:29 +0100

    exo termine

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 require_relative "../views/post_view.rb"
+require_relative "../models/post.rb"
+
 
 class PostsController
+
   def initialize
     @view = PostView.new
   end
@@ -9,25 +12,49 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    @posts = Post.all
+    @view.print_posts(@posts)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    title = @view.ask_for_title
+    url = @view.ask_for_url
+    votes = 0
+    post = Post.new(title: title, url: url, votes: votes)
+    post.save
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    title = @view.ask_for_title
+    post = Post.find_by(title: title)
+    post.title = @view.ask_for_new_title
+    post.url = @view.ask_for_new_url
+    post.save
+    # post.update(title: '#{new_title}', url: '#{new_url}')
+    # post.save
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    title = @view.ask_for_title
+    post = Post.find_by(title: title)
+    post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    title = @view.ask_for_title
+    post = Post.find_by(title: title)
+    post.votes += 1
+    post.save
   end
+
 end
+
+
--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
@@ -1,3 +1,57 @@
 class PostView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+
+  def print_posts(posts)
+    posts.each do |post|
+     # stars = "*" * post.votes
+     # date = post.date.strftime("%d/%m %H:%M")
+     puts "#{post.title}, #{post.url} (votes: #{post.votes})"
+    end
+  end
+
+ def ask_for_title
+   puts "Title?"
+   print "> "
+   gets.chomp
+ end
+
+ def ask_for_url
+   puts "URL?"
+   print "> "
+   gets.chomp
+ end
+
+ def ask_for_new_title
+   puts "New title?"
+   print "> "
+   gets.chomp
+ end
+
+ def ask_for_new_url
+   puts "New URL?"
+   print "> "
+   gets.chomp
+ end
+
+
+ def print_post_not_found(id)
+   puts "Could not find any post with id '#{id}'"
+ end
+
+ def print_successfully_created(post)
+   puts "Post '#{post.title}' has been successfully created"
+ end
+
+ def print_successfully_updated(post)
+   puts "Post '#{post.title}' has been successfully updated"
+ end
+
+ def print_successfully_destroyed(post)
+   puts "Post '#{post.title}' has been successfully destroyed"
+ end
+
+ def print_successfully_upvoted(post)
+   puts "Post '#{post.title}' has been successfully upvoted"
+ end
+
 end
