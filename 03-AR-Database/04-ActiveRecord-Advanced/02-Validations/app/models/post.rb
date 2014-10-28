97e8475011a95bb9dbe6bfad2ad24937c989f6ec
amontcoudiol <amontcoudiol@gmail.com>
2014-10-28 12:18:41 +0100

    exo termine

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :name, :url, :user
+  validates :name, length: { minimum: 5 }, uniqueness: { case_sensitive: false, message: "has already been taken" }
+  # validates :url, format: { with: /\Ahttp\:\/\/www\..*\.(org|com)\z/, message: "invalid url" } (Ne fonctionne pas avec cette regexp qui est pourtant bonne)
+  validates :url, format: { with: /\Ahttp:\/\/.+\.[a-zA-Z]{2,4}/, message: "invalid url" }
   belongs_to :user
 
   # TODO: Add some validation
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username, :email
+  validates :username, uniqueness: { case_sensitive: false, message: "has already been taken" }
+  # validates :email, format: { with: /\A.*@.*\.com\z/, message: "invalid email" }    (Ne fonctionne pas avec cette regexp qui est pourtant bonne)
+  validates :email, format: { with: /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/, message: "invalid email" }
   has_many :posts
 
   # TODO: Add some validation
