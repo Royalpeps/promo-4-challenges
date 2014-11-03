0c5bdd1eb9318d004b317586e903ec3f873a83ce
amontcoudiol <amontcoudiol@gmail.com>
2014-11-03 15:49:06 +0100

    yo

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,19 @@
   * TODO: add your code here!
   *
 */
+$(function() {
+  $("#message-wrapper").hide();
+  $("#message-wrapper").delay( 500 ).slideDown("slow");
+  $("#message-wrapper").on('click', function(the_event) {
+    $(this).slideUp();
+  });
+
+  $( "#banner" ).mousemove(function(event) {
+    if (event.clientY < 200){
+      $("#message-wrapper").show();
+    }
+    else { $("#message-wrapper").hide();}
+  });
+});
+
+
